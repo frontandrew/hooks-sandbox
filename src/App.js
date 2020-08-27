d14c604c1e9c8392bebc4f10ca38a6e9a76d08c8
Andrew Bubnov <bubnov@ujin.tech>
2020-08-27 18:23:22 +0500

    Fourth hook in progress...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-function App() {
+const veryComplexCompute = (num) => {
+  let i = 0;
+  while (i < 1000000000) i++;
+  return num;
+};
+
+const App = () => {
+  const [number, setNumber] = useState(42);
+  const [colored, setColored] = useState(false);
+
+  const computed = veryComplexCompute(number);
+
+  const btnStyle = { width: '30%' };
+
   return (
-    <div>
-      
+    <div
+      style={{
+        width: '100%',
+        maxWidth: '400px',
+        margin: '40px auto',
+      }}>
+      <h2 style={{ textAlign: 'center' }}>Calc value: {number}</h2>
+      <button
+        style={btnStyle}
+        className='btn btn-success'
+        onClick={() => setNumber((prev) => prev + 1)}>
+        ADD
+      </button>
+      <button
+        style={btnStyle}
+        className='btn btn-danger'
+        onClick={() => setNumber((prev) => prev - 1)}>
+        DEL
+      </button>
+      <button
+        style={btnStyle}
+        className='btn btn-secondary'
+        onClick={() => setColored((prev) => !prev)}>
+        COLOR
+      </button>
     </div>
   );
-}
+};
 
 export default App;
